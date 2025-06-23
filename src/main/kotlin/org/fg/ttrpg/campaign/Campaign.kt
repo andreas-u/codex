f45ce8a6aa8d2d474043374255a4886b675b50a1
andreas-u <134019765+andreas-u@users.noreply.github.com>
2025-06-23 20:53:11 +0200

    Remove JPA annotations from domain models

--- src/main/kotlin/org/fg/ttrpg/campaign/Campaign.kt
+++ src/main/kotlin/org/fg/ttrpg/campaign/Campaign.kt
@@ -1,29 +1,13 @@
 package org.fg.ttrpg.campaign
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToMany
 import java.time.Instant
 import java.util.UUID
 
-@Entity
-class Campaign  {
-    @Id
+class Campaign {
     var id: UUID? = null
-    @Column(name = "name")
     var title: String? = null
-
-    @Column(name = "started_on")
     var startedOn: Instant? = null
-
-    @ManyToOne
     var gm: org.fg.ttrpg.account.GM? = null
-
-    @ManyToOne
     var setting: org.fg.ttrpg.setting.Setting? = null
-
-    @OneToMany(mappedBy = "campaign")
     var objects: MutableList<CampaignObject> = mutableListOf()
 }
