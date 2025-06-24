8bde75c6886a86a43220d9c00f21bfbfe375777f
andreas-u <134019765+andreas-u@users.noreply.github.com>
2025-06-24 16:23:57 +0200

    Add TemplateService and update TemplateResource

--- src/main/kotlin/org/fg/ttrpg/setting/resource/TemplateResource.kt
+++ src/main/kotlin/org/fg/ttrpg/setting/resource/TemplateResource.kt
@@ -9,13 +9,13 @@ import jakarta.ws.rs.core.MediaType
 import org.eclipse.microprofile.jwt.JsonWebToken
 import org.fg.ttrpg.common.dto.TemplateDTO
 import org.fg.ttrpg.setting.Template
-import org.fg.ttrpg.setting.TemplateRepository
+import org.fg.ttrpg.setting.TemplateService
 import java.util.UUID
 
 @Path("/api/templates")
 @Produces(MediaType.APPLICATION_JSON)
 class TemplateResource @Inject constructor(
-    private val templates: TemplateRepository,
+    private val service: TemplateService,
     private val jwt: JsonWebToken
 ) {
     private fun gmId() = UUID.fromString(jwt.getClaim("gmId"))
@@ -27,9 +27,9 @@ class TemplateResource @Inject constructor(
     ): List<TemplateDTO> {
         val gid = gmId()
         val list = when {
-            genreId != null && type == null -> templates.listByGenre(genreId.toString())
-            type != null && genreId == null -> templates.listByType(type)
-            else -> templates.listByGm(gid)
+            genreId != null && type == null -> service.listByGenre(genreId.toString())
+            type != null && genreId == null -> service.listByType(type)
+            else -> service.listAll(gid)
         }
         return list.map { it.toDto() }
     }
